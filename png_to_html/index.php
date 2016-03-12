173f5a0cecd541bbdbaf0fd7843388f1c53b1b7f
hardik797 <hardik797@gmail.com>
2016-03-12 14:14:09 +0530

    added graphics

---
+++ png_to_html/index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Fit Identity</title>
+	<meta encoding="utf-8" name="keywords" content="html,css,javascript,jquery" >
+	<link rel="stylesheet" type="text/css" href="css/main.css" />
+	<link rel="shortcut icon"  href="img/img6.png">
+
+</head>
+<body>
+	<!-- header -->
+	<header class="align">
+		<div id="header">
+			<div id="logo">
+				<ul>
+					<li><img src="img/logo@2x.png"></li>
+					<li id="process"><img src="img/process.png"></li>
+					<li class="gray">THE PROCESS</li>
+				</ul>
+			</div>
+			<span class="clear"></span>
+			<div class="menu">
+			<ul id="menu-ul">
+				<li id="menu-li" class="navy"><a href="#">THINK</a></li>
+				<li id="menu-li" class="green"><a href="#">EAT</a></li>
+				<li id="menu-li" class="orange"><a href="#">MOVE</a></li>
+				<li id="menu-li" ><a href="#">RESOURCES</a></li>
+				<li id="menu-li"><a href="#">SHOP NOW</a></li>
+			</ul>
+			</div>
+		</div>
+	</header>
+	<!-- container -->
+	<div class="align">
+		<container >
+			<div class="container">
+				<ul>
+					<li class="white">THINK SMARTER.</li>
+					<li class="white">EAT HEALTHIER.</li>
+					<li class="white">MOVE BETTER.</li>
+					<li class="white"><button>I WANT TO START TODAY!</button></li>
+					<li class="white">MOVE BETTER.</li>
+				</ul>
+				<div id="video">
+					<img src="img/video.png">
+				</div>
+			</div>
+		</container>
+	</div>
+	<!-- page -->
+	<div class="align">
+		<page>
+			<div id="page">
+				<h3>TAKE YOUR <span>FREE</span>FIT IDENTITY ASSESSMENT TODAY! 
+					<input type="text" placeholder="email@address.com"/>
+					<button>GET STARTED</button>
+					<h5>Enter you email now to get started!</h5>
+				</h3>
+
+			</div>
+		</page>
+	</div>
+	<div class="align">
+		<div id="fill" >
+			<div class="fill-navy"></div>
+			<div class="fill-green"></div>
+			<div class="fill-orange"></div>
+		</div>
+	</div>
+	<div class="align">
+		<div id="dots">
+			<h2>LEARN TO CONNECT THE DOTS</h2>
+			<div id="circle">
+				<div id="circle-navy">THINK</div>
+				<div id="circle-green">EAT</div>
+				<div id="circle-orange">MOVE</div>
+			</div>
+		</div>
+	</div>
+	<!-- portfolio -->
+	<div class="align">
+		<div class="portfolio" >
+			<div class="think">
+				<div class="think-message">
+					<p>How you</p>
+					<h2>THINK</h2>
+				</div>
+				<img src="img/img1.png">
+				<div class="think-content">
+					<img src="img/dots.png">
+				</div>
+			</div>
+			<div class="eat">
+				<div class="eat-message">
+					<div class="eat-circle">
+						<p>How you</p>
+						<h2>EAT</h2>
+					</div>
+				</div>
+				<img src="img/img2.png">
+			</div>
+			<div class="move">
+				<div class="move-message">
+					<p>How you</p>
+					<h2>MOVE</h2>
+				</div>
+				<div class="move-content">
+					<img src="img/dots.png">
+				</div>
+				<img src="img/img3.png">
+			</div>
+		</div>
+	</div>
+	<div class="align">
+		<div id="port">
+			<h3>TAKE YOUR <span>FREE</span>FIT IDENTITY ASSESSMENT TODAY! 
+				<input type="text" placeholder="email@address.com"/>
+				<button>GET STARTED</button>
+				<h5>Enter you email now to get started!</h5>
+			</h3>
+		</div>
+	</div>
+	<!-- footer -->
+	<div class="align">
+		<footer>
+			<div class="footer">
+				
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
