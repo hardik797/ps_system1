fc9aba4d27f9494233b0999fef32b6b2a6bbe54d
Hardik Karmur <hardik797@users.noreply.github.com>
2016-03-17 22:43:35 +0530

    final live page done index

--- png_to_html/index.php
+++ png_to_html/index.php
@@ -1,256 +1,282 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Fit Identity</title>
-	<meta encoding="utf-8" name="keywords" content="html,css,javascript,jquery">
-	<link rel="stylesheet" type="text/css" href="css/main.css" />
-	<link rel="shortcut icon"  href="img/img6.png">
+    <title>Fit Identity</title>
+    <meta encoding="utf-8" name="keywords" content="html,css,javascript,jquery">
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <link rel="shortcut icon"  href="img/img6.png">
 
 </head>
-<body>
-	<!-- header -->
-	<header class="align">
-		<div id="header">
-			<div id="logo">
-				<ul>
-					<li><img src="img/logo@2x.png"></li>
-					<li id="process"><img src="img/process.png"></li>
-					<li class="gray">THE PROCESS</li>
-				</ul>
-			</div>
-			<div class="menu">
-				<ul>
-					<li ><a class="navy" href="#">THINK</a></li>
-					<li ><a class="green" href="#">EAT</a></li>
-					<li ><a class="orange" href="#">MOVE</a></li>
-					<li><a href="#">RESOURCES</a></li>
-					<li><a href="#">SHOP NOW</a></li>
-					</ul>
-			</div>
-		</div>
-	</header>
-    <section class="align">
-        <!-- container -->        
-            <container >
-                <div class="container">
+<body> 
+    <!--main container -->
+    <div class="main-container">
+        <!-- header -->
+        <header>
+            <div class="head-sizing">
+                <div class="logo">
                     <ul>
-                        <li class="white">THINK SMARTER.</li>
-                        <li class="white">EAT HEALTHIER.</li>
-                        <li class="white">MOVE BETTER.</li>
-                        <li class="white"><button>I WANT TO START TODAY!</button></li>
-                        <li class="white">MOVE BETTER.</li>
+                        <li><img src="img/logo-main.png"></li>
+                        <li class="process"><img src="img/process.png"></li>
+                        <li class="small-font">THE PROCESS</li>
                     </ul>
-                    <div id="video">
-                        <img src="img/video.png">
-        
-    				</div>
-    			</div>
-    		</container>    	
-    	<!-- page -->    	
-    		<page>
-    			<div id="page">
-    				<h3>TAKE YOUR <span>FREE</span>FIT IDENTITY ASSESSMENT TODAY! 
-    					<input type="text" placeholder="email@address.com"/>
-    					<button>GET STARTED</button>
-    					<h5>Enter you email now to get started!</h5>
-    				</h3>
-
-    			</div>
-    		</page>    	    
-    		<div id="fill" >
-    			<div class="fill-navy"></div>
-    			<div class="fill-green"></div>
-    			<div class="fill-orange"></div>
-    		</div>    	
-    	   <div id="dots">
-    			<h2>LEARN TO CONNECT THE DOTS</h2>
-    			<div id="circle">
-    				<div id="circle-navy">THINK</div>
-    				<div id="circle-green">EAT</div>
-    				<div id="circle-orange">MOVE</div>
-    			</div>
-    		</div>
-    	<!-- portfolio -->
-    		<div class="portfolio" >
-    			<div class="think">
-    				<div class="think-message">
-    					<p>How you</p>
-    					<h2>THINK</h2>
-    				</div>
-    				<img src="img/img1.png">
-    				<div class="think-content">
-    					<img src="img/dots.png">
-    				</div>
-    			</div>
-    			<div class="eat">
-    				<div class="eat-message">
-    					<div class="eat-circle">
-    						<p>How you</p>
-    						<h2>EAT</h2>
-    					</div>
-    				</div>
-    				<img src="img/img2.png">
-    			</div>
-    			<div class="move">
-    				<div class="move-message">
-    					<p>How you</p>
-    					<h2>MOVE</h2>
-    				</div>
-    				<div class="move-content">
-    					<img src="img/dots.png">
-    				</div>
-    				<img src="img/img3.png">
-    			</div>
-    		</div>
-    	</div>
-    	<div class="align">
-    		<div id="port">
-    			<h3>TAKE YOUR <span>FREE</span>FIT IDENTITY ASSESSMENT TODAY! 
-    				<input type="text" placeholder="email@address.com"/>
-    				<button>GET STARTED</button>
-    				<h5>Enter you email now to get started!</h5>
-    			</h3>
-    		</div>
-    	<!-- banners -->
-    		<div class="banner">
-    			<div>
-    				<img src="img/img4.png">
-    			</div>
-    			<div>
-    				<h1>WHAT IS YOUR FIT IDENTITY?</h1>
-    				
-    				<p>
-    					Millions of people around the world are
-    					focused on getting and staying fit to live
-    					their healthiest lives.
-    				</p>
-    				
-    				<div>
-    					<i>✔
-    						<p id="small">
-    							Do you ever feel like you just can't achieve-or-maintain-the
-    							level of health and fitness you really want?
-    						</p>
-    					</i>
-    					<i>✔
-    						<p id="small">
-    							Have you followed dozens of eating plans and workout
-    							programs, only to find that they don't work as well for you
-    							as they do for others?
-    						</p>
-    					</i>
-    				</div>
-    				
-    				<p>
-    					Are you ready to get fit and stay fit?
-    				</p>
-    				
-    				<as>
-    					Learn to connect the dots between the three key dimensions of 
-    					your health and get in the best shape of your life.
-    				</as>
-    				
-    				<button>
-    					GET STARTED
-    				</button>
-    			</div>
-    		</div>		
-    		<div class="banner1">
-    			<div>
-    				<img src="img/img5.png">
-    			</div>
-    			<div class="banner1-center">
-    				<b>MIKE KARPENKO</b>
-    			</div>
-    			<div id="small-text">
-    				<i>
-    					"This is a testimonial.Praesent commodo cursus magna,
-    					vel scelerisquenisl consectetur et. Maecenas sed diam."
-    				</i>
-    			</div>
-    			<div class="banner1-center-span">
-    				<span>-PERSON NAME</span>
-    			</div>
-    			<div class="banner1-center-p">
-    				<p>
-    					Mike Karpenko is a transformational fitness coach who has
-    					changed the lives of people all over the world. His innovative
-    					Fit Identity program is the clumination of decades of
-    					leadership in the health and fitness industry.
-    				</p>
-    			</div>
-    			<div class="banner1-center">
-    				<button>
-    					LEARN MORE
-    				</button>
-    			</div>
-    		</div>
-    	<!-- banner-3-->
-            <div class="banner-3">
-                <b>
-                    GET YOUR FIT IDENTITY TODAY!
-                </b>
-                <div>
-                    <img src="img/img6.png">
-                    <div class="banner-3-right">
-                        <i id="normal-text">
-                            Start seeing results malesuada magna
-                            mollis euismod. Curabitur tempus porttitor.
-                        </i>
-                        <p id="tiny-text">
-                            Mike Karpenko is a transformational fitness coach who has
-                            changed the lives of people all over the world. His innovative
-                            Fit Identity program is the clumination of decades of
-                            leadership in the health and fitness industry.
-                        </p>
-                        <div class="banner-3-right">
-                            <img id="banner-3-right-img" src="img/btn-access.png">
-                        </div>
-                    </div>
-                </div>  
-    		</div>		
-    	</section>
-	<!-- footer -->
-	<footer class="align">
-        <div class="footer">
-            <div id="border">
+                </div>
+                <div class="menu">
+                    <ul>
+                        <li><a class="navy" href="#">THINK</a></li>
+                        <li><a class="green" href="#">EAT</a></li>
+                        <li><a class="orange" href="#">MOVE</a></li>
+                        <li><a href="#">RESOURCES</a></li>
+                        <li><a href="#">SHOP NOW</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        <!-- container -->        
+        <div class="banner-main">
+            <div>
+                <img src="img/slide1.png">
+            </div>
+            <ul class="banner-main">
+                <li>THINK SMARTER.</li>
+                <li>EAT HEALTHIER.</li>
+                <li>MOVE BETTER.</li>
+                <li><button>I WANT TO START TODAY!</button></li>
+            </ul>
+            <div class="video">
+                <img src="img/video.png">        
+            </div>
+        </div>
+        <!--GET START-2 -->
+        <div class="get-start-1">
+            <div class="get-started-1">
                 <h3>TAKE YOUR <span>FREE</span>FIT IDENTITY ASSESSMENT TODAY! 
                     <input type="text" placeholder="email@address.com"/>
-                    <button>GET STARTED</button>                   
+                    <button>GET STARTED</button>
+                    <h5>Enter you email now to get started!</h5>
                 </h3>
-                <h5>Enter you email now to get started!</h5>        
             </div>
-            <div id="menu">
-                <ul>
-                    <li><a href="#">ABOUT MIKE</a></li>
-                    <li><a href="#">TERMS/PRIVACY</a></li>
-                    <li><a href="#">CONTACT</a></li>
-                </ul>
-                <h5>© 2014 Fit Identity.All rights reserved.</h5>
+        </div>  
+        <!-- fill line -->  
+        <div class="fill" >
+            <div class="fill-navy"></div>
+            <div class="fill-green"></div>
+            <div class="fill-orange"></div>
+        </div>      
+        <!--learn to connect -->
+        <div class="dots">
+            <h2>LEARN TO CONNECT THE DOTS</h2>
+            <div>
+                <img src="img/arrow_down.png">
             </div>
-            <div id="icons">
-                <ul>
-                    <li>
-                        <a href="#"><img src="img/twitter.png"></a>
-                    </li>
-                    <li>
-                        <a href="#"><img src="img/fb.png"></a>
-                    </li>
-                    <li>
-                        <a href="#"><img src="img/instagram.png"></a>
-                    </li>
-                    <li>
-                        <a href="#"><img src="img/plus.png"></a>
-                    </li>
-                </ul>
-                <input type="text" placeholder="Search" />
+            <div class="circle">
+                <div class="circle-navy">THINK</div>
+                <div class="circle-navy-dots">
+                    <img src="img/dots_horizontal.png">
+                </div>
+                <div class="circle-green">EAT</div>
+                <div class="circle-orange">MOVE</div>
+                <div class="circle-orange-dots">
+                    <img src="img/dots_horizontal.png">
+                </div>
             </div>
-        </div>               
-        <div id="fill">
-            <div id="fill-navy"></div>
-            <div id="fill-green"></div>
-            <div id="fill-orange"></div>
         </div>
-    </footer>
+        <!-- portfolio -->
+        <div class="portfolio">
+            <div class="think">
+                <div class="think-message">
+                    <p>How you</p>
+                    <h2>THINK</h2>
+                </div>
+                <div>
+                    <img src="img/img1.png">
+                </div>
+                <div class="think-content">
+                    <img src="img/dots.png">
+                </div>
+            </div>
+            <div class="eat">
+                <div class="eat-message">
+                    <div class="eat-circle">
+                        <p>How you</p>
+                        <h2>EAT</h2>
+                    </div>
+                </div>
+                <div>
+                    <img src="img/img2.png">
+                </div>
+            </div>
+            <div class="move">
+                <div class="move-message">
+                    <p>How you</p>
+                    <h2>MOVE</h2>
+                </div>
+                <div class="move-content">
+                    <img src="img/dots.png">
+                </div>
+                <div>
+                    <img src="img/img3.png">
+                </div>
+            </div>
+        </div>
+        <div class="clear" ></div>
+        <!--GET START-2 -->
+        <div class="get-start-2">
+            <div class="get-started-2">
+                <h3>TAKE YOUR <span>FREE</span>FIT IDENTITY ASSESSMENT TODAY! 
+                    <input type="text" placeholder="email@address.com"/>
+                    <button>GET STARTED</button>
+                    <h5>Enter you email now to get started!</h5>
+                </h3>
+            </div>
+        </div>
+        <!-- section-1 -->
+        <div class="section-1">
+            <div>
+                <img src="img/img4.png">
+            </div>
+            <div>
+                <h1>WHAT IS YOUR FIT IDENTITY?</h1>
+
+                <p>
+                    Millions of people around the world are
+                    focused on getting and staying fit to live
+                    their healthiest lives.
+                </p>
+                <div class="paragraph-1">
+                    <div class="icon-1">✔</div>
+                    <p>
+                        Do you ever feel like you just can't achieve-or-maintain-the
+                        level of health and fitness you really want?    
+                    </p>   
+                </div>
+                <div class="paragraph-2">
+                    <div class="icon-2">✔</div>
+                    <p>                        
+                        Have you followed dozens of eating plans and workout
+                        programs, only to find that they don't work as well for you
+                        as they do for others?
+                    </p>
+                </div>
+                <div class="paragraph-3">
+                    Are you ready to get fit and stay fit?
+                </div>
+                <span>
+                    Learn to connect the dots between the three key dimensions of 
+                    your health and get in the best shape of your life.
+                </span>
+                <button>
+                    GET STARTED
+                </button>
+            </div>
+        </div>      
+        <!-- section-2 -->
+        <div class="section-2">
+            <div class="section-2-content">
+                <div class="section-2-center">
+                    <b>MIKE KARPENKO</b>
+                </div>
+                <div class="text-small">
+                    <p>
+                        <i>
+                           "This is a testimonial.Praesent commodo cursus magna,
+                            vel scelerisquenisl consectetur et. Maecenas sed diam."
+                        </i>
+                    </p>
+                </div>
+                <div class="text-small">
+                    <span>-PERSON NAME</span>
+                </div>
+                <div class="section-2-center-p">
+                    <p>
+                        Mike Karpenko is a transformational fitness coach who has
+                        changed the lives of people all over the world. His innovative
+                        Fit Identity program is the clumination of decades of
+                        leadership in the health and fitness industry.
+                    </p>
+                </div>
+                <div class="section-2-center">
+                    <button>
+                        LEARN MORE
+                    </button>
+                </div>
+            </div>
+            <div>
+                <img src="img/img5.png">
+            </div>
+        </div>
+        <!-- section-3 -->
+        <div class="section-3">
+	        <div class="section-3-big-font">
+				<b>GET YOUR FIT IDENTITY TODAY!</b>
+			</div>
+			<div class="section-3-content-left">
+            	<div>
+                	<img src="img/img6.png">
+				</div>
+			</div>
+            <div class="section-3-content-right">
+            	<div class="section-3-normal-text">
+                    <p>
+                        Start seeing results malesuada magna
+                        mollis euismod. Curabitur tempus porttitor.
+                    </p>
+                </div>
+                <div class="section-3-tiny-text" 
+                    <p>
+                        Mike Karpenko is a transformational fitness coach who has
+                        changed the lives of people all over the world. His innovative
+                        Fit Identity program is the clumination of decades of
+                        leadership in the health and fitness industry.
+                    </p>
+				</div>
+				<div class="section-3-right">
+					<img src="img/btn-access.png">
+				</div>
+			</div>
+		</div>
+        <!-- footer -->
+        <footer>
+            <div class="footer">
+                <div class="get-started-3">
+                    <h3>TAKE YOUR <span>FREE</span>FIT IDENTITY ASSESSMENT TODAY! 
+                        <input type="text" placeholder="email@address.com"/>
+                        <button>GET STARTED</button>                   
+                    </h3>
+                    <h5>Enter you email now to get started!</h5>        
+                </div>
+                <div class="footer-menu">
+                    <ul>
+                        <li><a href="#">ABOUT MIKE</a></li>
+                        <li><a href="#">TERMS/PRIVACY</a></li>
+                        <li><a href="#">CONTACT</a></li>
+                    </ul>
+                    <h5> © 2014 Fit Identity.All rights reserved.</h5>
+                </div>
+                <div class="footer-icons">
+                    <ul>
+                        <li>
+                            <a href="#"><img src="img/twitter.png"></a>
+                        </li>
+                        <li>
+                            <a href="#"><img src="img/fb.png"></a>
+                        </li>
+                        <li>
+                            <a href="#"><img src="img/instagram.png"></a>
+                        </li>
+                        <li>
+                            <a href="#"><img src="img/plus.png"></a>
+                        </li>
+                    </ul>
+                    <input type="text" placeholder="Search" />
+                </div>
+            </div>               
+            <div class="footer-icons-fill">
+                <div id="footer-fill-navy"></div>
+                <div id="footer-fill-green"></div>
+                <div id="footer-fill-orange"></div>
+            </div>
+        </footer>
+    </div>
 </body>
 </html>
